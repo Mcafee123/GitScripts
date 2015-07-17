a81b2c60872db3dd7888c2bd3127c22255e77826
Mcafee123 <martin@affolter.net>
2015-07-17 13:04:33 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -1,5 +1,6 @@
  param (
-    [string]$gitPath = "C:\Program Files (x86)\Git\bin\git.exe"
+    [string]$gitPath = "C:\Program Files (x86)\Git\bin\git.exe",
+	[string]$branchName = "master"
  )
 
  function Log([string]$msg, [string]$status = "NORMAL")
@@ -21,10 +22,10 @@
  function CheckChanges()
  {
     Log "check for changes"
-    & $gitPath diff --shortstat --exit-code origin/master
+    & $gitPath diff --shortstat --exit-code origin/$branchName
     if ($LASTEXITCODE -ne 1)
     {
-        Log "=== THIS IS NO ERROR === no changes found in master === THIS IS NO ERROR ===" "WARNING"
+        Log "=== THIS IS NO ERROR === no changes found in $branchName === THIS IS NO ERROR ===" "WARNING"
     }
  }
 
@@ -33,22 +34,33 @@ function Cleanup()
     Log "reset"
     & $gitPath reset --hard HEAD
     Log "clean"
-    & $gitPath clean --force -x -d
+    & $gitPath clean --force -x -d -e cmn.Dlls/
     if($LASTEXITCODE -ne 0)
     {
-        Log "cleanup failed" "ERROR"
-        exit 1
+        Log "cleanup failed" "WARNING"
+        #exit 1
     }
 }
 
-function MergeMasterChanges()
+function MergeChanges()
 {
     Log "merge changes to build_history branch"
-    & $gitPath merge -X theirs --no-ff --no-commit origin/master
+    & $gitPath merge -X theirs --no-ff --no-commit origin/$branchName
     if ($LASTEXITCODE -ne 0)
     {
-        Log "cold not merge changes" "ERROR"
-        exit 1
+        Log "cold not merge changes, perhaps something was deleted, try adding the deletion" "WARNING"
+		& $gitPath add -u
+		if ($LASTEXITCODE -ne 0)
+		{
+			Log "adding deletion failed" "ERROR"
+			exit 1
+		}
+		& $gitPath commit -m "Merge remote-tracking branch 'origin/$branchName' into build-history, deleted files as in remote"
+		if ($LASTEXITCODE -ne 0)
+		{
+			Log "committing deletion failed" "ERROR"
+			exit 1
+		}
     }
 }
 
@@ -58,11 +70,11 @@ function MergeMasterChanges()
 
  #fetch changes
 Fetch
-# check for changes in master
+# check for changes
 CheckChanges
 #cleanup
 Cleanup
 #merge changes into "build_history" branch
-MergeMasterChanges
+MergeChanges
 Log "prepare working copy OK"
 exit 0
